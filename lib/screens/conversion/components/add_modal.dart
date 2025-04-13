32c9ed2637902ed29aace95b7b56aa483fca28e8
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2025-04-13 14:12:43 -0300

    feat: add loading state to conversion modal and improve UI responsiveness

--- lib/screens/conversion/components/add_modal.dart
+++ lib/screens/conversion/components/add_modal.dart
@@ -12,6 +12,7 @@ class ConversionAddController extends GetxController {
   var result = 0.0.obs;
   var amount = 0.0.obs;
   var currencies = <String>[].obs;
+  var isLoading = false.obs;
 
   final GetCurrenciesService _getCurrenciesService = GetCurrenciesService();
   final GetRatePairConversionService _getRatePairConversionService =
@@ -50,10 +51,13 @@ class ConversionAddController extends GetxController {
         toCurrency.value.isNotEmpty &&
         fromCurrency.value != toCurrency.value) {
       try {
+        isLoading.value = true;
+
         rate.value = await _getRatePairConversionService.get(
           fromCurrency.value,
           toCurrency.value,
         );
+
         calculateResult();
       } catch (e) {
         Get.snackbar(
@@ -67,6 +71,8 @@ class ConversionAddController extends GetxController {
           margin: const EdgeInsets.all(16.0),
           borderRadius: 8.0,
         );
+      } finally {
+        isLoading.value = false;
       }
     }
   }
@@ -127,168 +133,194 @@ class ConversionAddModal extends StatelessWidget {
     return Dialog(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Obx(
-          () =>
-              addController.currencies.isEmpty
-                  ? const Center(child: CircularProgressIndicator())
-                  : Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Adicionar Conversão',
-                        style: Theme.of(
-                          context,
-                        ).textTheme.bodyLarge?.copyWith(fontSize: 18),
-                      ),
-                      const SizedBox(height: 16),
-                      const SizedBox(height: 8),
-                      SizedBox(
-                        height: 40,
-                        child: DropdownButton<String>(
-                          value:
-                              addController.fromCurrency.value.isEmpty
-                                  ? null
-                                  : addController.fromCurrency.value,
-                          hint: Text(
-                            'Selecione a Moeda de Origem',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color:
-                                  Theme.of(context).textTheme.bodyMedium?.color,
-                            ),
-                          ),
-                          dropdownColor:
-                              Theme.of(context).scaffoldBackgroundColor,
-                          items:
-                              addController.currencies
-                                  .map(
-                                    (currency) => DropdownMenuItem(
-                                      value: currency,
-                                      child: Text(
-                                        currency,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyMedium
-                                            ?.copyWith(fontSize: 14),
-                                      ),
-                                    ),
-                                  )
-                                  .toList(),
-                          onChanged: (value) {
-                            if (value != null) {
-                              addController.fromCurrency.value = value;
-                              addController.fetchRate();
-                            }
-                          },
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      SizedBox(
-                        height: 40,
-                        child: DropdownButton<String>(
-                          value:
-                              addController.toCurrency.value.isEmpty
-                                  ? null
-                                  : addController.toCurrency.value,
-                          hint: Text(
-                            'Selecione a Moeda de Destino',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color:
-                                  Theme.of(context).textTheme.bodyMedium?.color,
-                            ),
-                          ),
-                          dropdownColor:
-                              Theme.of(context).scaffoldBackgroundColor,
-                          items:
-                              addController.currencies
-                                  .where(
-                                    (currency) =>
-                                        currency !=
-                                        addController.fromCurrency.value,
-                                  )
-                                  .map(
-                                    (currency) => DropdownMenuItem(
-                                      value: currency,
-                                      child: Text(
-                                        currency,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyMedium
-                                            ?.copyWith(fontSize: 14),
-                                      ),
-                                    ),
-                                  )
-                                  .toList(),
-                          onChanged: (value) {
-                            if (value != null) {
-                              addController.toCurrency.value = value;
-                              addController.fetchRate();
-                            }
-                          },
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      SizedBox(
-                        height: 40,
-                        child: TextField(
-                          style: Theme.of(
-                            context,
-                          ).textTheme.bodyMedium?.copyWith(fontSize: 14),
-                          decoration: InputDecoration(
-                            labelText: 'Quantidade',
-                            labelStyle: Theme.of(
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Obx(
+              () =>
+                  addController.currencies.isEmpty
+                      ? const Center(child: CircularProgressIndicator())
+                      : Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Adicionar Conversão',
+                            style: Theme.of(
                               context,
-                            ).textTheme.bodyMedium?.copyWith(fontSize: 14),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color:
-                                    Theme.of(
-                                      context,
-                                    ).textTheme.bodyMedium?.color ??
-                                    Colors.grey,
+                            ).textTheme.bodyLarge?.copyWith(fontSize: 18),
+                          ),
+                          const SizedBox(height: 16),
+                          SizedBox(
+                            height: 40,
+                            child: DropdownButton<String>(
+                              value:
+                                  addController.fromCurrency.value.isEmpty
+                                      ? null
+                                      : addController.fromCurrency.value,
+                              hint: Text(
+                                'Selecione a Moeda de Origem',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).textTheme.bodyMedium?.color,
+                                ),
                               ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Theme.of(context).primaryColor,
-                              ),
+                              dropdownColor:
+                                  Theme.of(context).scaffoldBackgroundColor,
+                              items:
+                                  addController.currencies
+                                      .map(
+                                        (currency) => DropdownMenuItem(
+                                          value: currency,
+                                          child: Text(
+                                            currency,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium
+                                                ?.copyWith(fontSize: 14),
+                                          ),
+                                        ),
+                                      )
+                                      .toList(),
+                              onChanged: (value) {
+                                if (value != null) {
+                                  addController.fromCurrency.value = value;
+                                  addController.fetchRate();
+                                }
+                              },
                             ),
                           ),
-                          keyboardType: TextInputType.number,
-                          onChanged: (value) {
-                            addController.amount.value =
-                                double.tryParse(value) ?? 0.0;
-                            addController.calculateResult();
-                          },
+                          const SizedBox(height: 8),
+                          SizedBox(
+                            height: 40,
+                            child: DropdownButton<String>(
+                              value:
+                                  addController.toCurrency.value.isEmpty
+                                      ? null
+                                      : addController.toCurrency.value,
+                              hint: Text(
+                                'Selecione a Moeda de Destino',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).textTheme.bodyMedium?.color,
+                                ),
+                              ),
+                              dropdownColor:
+                                  Theme.of(context).scaffoldBackgroundColor,
+                              items:
+                                  addController.currencies
+                                      .where(
+                                        (currency) =>
+                                            currency !=
+                                            addController.fromCurrency.value,
+                                      )
+                                      .map(
+                                        (currency) => DropdownMenuItem(
+                                          value: currency,
+                                          child: Text(
+                                            currency,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium
+                                                ?.copyWith(fontSize: 14),
+                                          ),
+                                        ),
+                                      )
+                                      .toList(),
+                              onChanged: (value) {
+                                if (value != null) {
+                                  addController.toCurrency.value = value;
+                                  addController.fetchRate();
+                                }
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          SizedBox(
+                            height: 40,
+                            child: TextField(
+                              style: Theme.of(
+                                context,
+                              ).textTheme.bodyMedium?.copyWith(fontSize: 14),
+                              decoration: InputDecoration(
+                                labelText: 'Quantidade',
+                                labelStyle: Theme.of(
+                                  context,
+                                ).textTheme.bodyMedium?.copyWith(fontSize: 14),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color:
+                                        Theme.of(
+                                          context,
+                                        ).textTheme.bodyMedium?.color ??
+                                        Colors.grey,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              ),
+                              keyboardType: TextInputType.number,
+                              onChanged: (value) {
+                                addController.amount.value =
+                                    double.tryParse(value) ?? 0.0;
+                                addController.calculateResult();
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Taxa: ${addController.rate.value.toStringAsFixed(2)}',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            'Resultado: ${addController.result.value.toStringAsFixed(2)}',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                            ),
+                            onPressed: () => addController.saveConversion(),
+                            child: const Text(
+                              'Salvar',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+            ),
+          ),
+          Obx(
+            () =>
+                addController.isLoading.value
+                    ? Positioned.fill(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: CircularProgressIndicator(
+                            color: Color(0xFFF0B90B),
+                            strokeWidth: 2,
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'Taxa: ${addController.rate.value.toStringAsFixed(2)}',
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        'Resultado: ${addController.result.value.toStringAsFixed(2)}',
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      const SizedBox(height: 16),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(context).primaryColor,
-                        ),
-                        onPressed: () => addController.saveConversion(),
-                        child: const Text(
-                          'Salvar',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ],
-                  ),
-        ),
+                    )
+                    : const SizedBox.shrink(),
+          ),
+        ],
       ),
     );
   }
